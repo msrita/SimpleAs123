da60b01408139d587403db609de515dd1deb3ac3
Rita <msritachart@gmail.com>
2018-06-09 14:03:39 -0700

    Initial Commit

---
+++ SimpleAs123/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleAs123
+//
+//  Created by Rita Hart on 6/9/18.
+//  Copyright © 2018 Rita Hart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
